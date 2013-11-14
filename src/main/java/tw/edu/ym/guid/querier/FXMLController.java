dd550a17d0e4e10aa799f880567c8265c7a2b8a7
WMW <wnameless@gmail.com>
2013-11-14 14:21:11 +0800

    Merge master

--- src/main/java/tw/edu/ym/guid/querier/FXMLController.java
+++ src/main/java/tw/edu/ym/guid/querier/FXMLController.java
@@ -55,7 +55,7 @@ public class FXMLController implements Initializable {
   private static final Logger log = LoggerFactory
       .getLogger(FXMLController.class);
 
-  public static final boolean DEV = false;
+  public static final boolean DEV = true;
   public static final String PROPS_PATH = "excel_manager.properties";
   public static final long AUTO_SHUTDOWN_TIME = 300000000000L;
 
